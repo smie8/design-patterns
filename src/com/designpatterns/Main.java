4ed93321e9569482d4d682d78698adfaa49749a9
smie8 <sami.eriksson@yahoo.fi>
2023-08-29 09:41:19 +0300

    Run miniducksimulator in main class.

---
+++ src/com/designpatterns/Main.java
@@ -0,0 +1,10 @@
+package com.designpatterns;
+
+import com.designpatterns.strategy.MiniDuckSimulator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MiniDuckSimulator.runSimulator();
+    }
+}
